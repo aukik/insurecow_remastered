f744a756202f87ffd811e6bad6502a0302d0d1f1
tahmid <tahmid.tf1@gmail.com>
2023-10-12 13:50:42 +0600

    logi api created

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Permission;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Cookie;
@@ -20,13 +21,54 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
+
+        $inputs['email'] = $request->input('email');
+        $inputs['phone'] = $request->input('phone');
+        $inputs['name'] = $request->input('name');
+        $inputs['password'] = $request->input('password');
+
+
+        // Check if the email or phone already exists in the database
+        if (User::where('email', $inputs['email'])->exists()) {
+            return response([
+                'message' => 'Email already exists'
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        if (User::where('phone', $inputs['phone'])->exists()) {
+            return response([
+                'message' => 'Phone number already exists'
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
         $user = User::create([
-            'name' => $request->input('name'),
-            'phone' => $request->input('phone'),
-            'password' => Hash::make($request->input('password'))
+            'name' => $inputs['name'],
+            'email' => $inputs['email'],
+            'phone' => $inputs['phone'],
+            'password' => Hash::make($inputs['password'])
         ]);
 
-        return $user;
+
+        if (Auth::attempt(['phone' => $request->input('phone'), 'password' => $request->input('password')])) {
+            $user = Auth::user();
+            $token = $user->createToken('token')->plainTextToken;
+            $cookie = cookie('jwt', $token, 60 * 24 * 30); // 30 days
+
+
+            Permission::create([
+                'user_id' => $user->id,
+                'role' => 'f'
+            ]);
+
+            return response([
+                'token' => $token,
+                'user' => $user,
+            ])->withCookie($cookie);
+        }
+
+        return response([
+            'message' => 'Invalid Credentials'
+        ], Response::HTTP_UNAUTHORIZED);
     }
 
     public function login(Request $request)
