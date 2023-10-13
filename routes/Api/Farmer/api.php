9546d1536038b090305b8aa8e58e3ce29a213794
Tahmid <tahmid15-6966@diu.edu.bd>
2023-10-13 17:05:10 +0600

    update

--- routes/Api/Farmer/api.php
+++ routes/Api/Farmer/api.php
@@ -22,10 +22,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
-Route::get('test', function (){
-    return "test";
-});
-
 Route::middleware(['auth:sanctum', 'api.farmer'])->group(function () {
     Route::get('user', [AuthController::class, 'user']);
     Route::post('logout', [AuthController::class, 'logout']);
