209d833aff4688dd4712a8b7c71f6a6e12e8b765
Tahmid <tahmid15-6966@diu.edu.bd>
2023-10-15 18:22:06 +0600

    update

--- routes/web.php
+++ routes/web.php
@@ -174,20 +174,17 @@ Route::middleware('auth')->group(function () {
 // -------------------------------------------------------------------- Payment Gateway --------------------------------------------------------------------
 
 
-Route::middleware('auth')->group(function (){
-    Route::get('/example1', [SslCommerzPaymentController::class, 'exampleEasyCheckout']);
-    Route::get('/example2', [SslCommerzPaymentController::class, 'exampleHostedCheckout']);
+Route::get('/example1', [SslCommerzPaymentController::class, 'exampleEasyCheckout']);
+Route::get('/example2', [SslCommerzPaymentController::class, 'exampleHostedCheckout']);
 
-    Route::post('/pay', [SslCommerzPaymentController::class, 'index'])->name("pay");
-    Route::post('/pay-via-ajax', [SslCommerzPaymentController::class, 'payViaAjax']);
+Route::post('/pay', [SslCommerzPaymentController::class, 'index'])->name("pay");
+Route::post('/pay-via-ajax', [SslCommerzPaymentController::class, 'payViaAjax']);
 
-    Route::post('/success', [SslCommerzPaymentController::class, 'success']);
-    Route::post('/fail', [SslCommerzPaymentController::class, 'fail']);
-    Route::post('/cancel', [SslCommerzPaymentController::class, 'cancel']);
+Route::post('/success', [SslCommerzPaymentController::class, 'success']);
+Route::post('/fail', [SslCommerzPaymentController::class, 'fail']);
+Route::post('/cancel', [SslCommerzPaymentController::class, 'cancel']);
 
-    Route::post('/ipn', [SslCommerzPaymentController::class, 'ipn']);
-//SSLCOMMERZ END
-});
+Route::post('/ipn', [SslCommerzPaymentController::class, 'ipn']);
 
 
 // -------------------------------------------------------------------- Payment Gateway --------------------------------------------------------------------
