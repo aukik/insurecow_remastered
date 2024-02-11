7c03d9075b4b965a0a4bd74865f5f452ea94aad0
tahmid <tahmid.tf1@gmail.com>
2024-02-11 14:33:24 +0600

    company request panel update

--- app/Http/Controllers/SuperAdmin/SuperAdminController.php
+++ app/Http/Controllers/SuperAdmin/SuperAdminController.php
@@ -3,16 +3,106 @@
 namespace App\Http\Controllers\SuperAdmin;
 
 use App\Http\Controllers\Controller;
+use App\Models\CattleRegReport;
+use App\Models\InsuranceRequest;
+use App\Models\Insured;
+use App\Models\Order;
+use App\Models\Package;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class SuperAdminController extends Controller
 {
-    public function profile(){
+    public function profile()
+    {
         return view('super-admin.admin-content.profile.profile');
     }
 
-    public function delete_company_request(Request $request){
-        return $request->all();
+    public function delete_company_request(Request $request)
+    {
+
+        $inputs = \request()->validate([
+            'user_id' => 'required'
+        ]);
+
+        $company_id = $inputs['user_id'];
+
+//  -------------------------- If any users belongs to this company --------------------------
+
+        $user = User::where('company_id', $company_id)->count();
+
+        if ($user > 0) {
+
+            session()->flash("warning", "Farmers belongs to this company exists, delete request failed");
+            return redirect()->route('sp.company_request');
+        }
+
+//  -------------------------- If any users belongs to this company --------------------------
+
+//  -------------------------- If any packages belongs to this company --------------------------
+
+        $package = Package::where('user_id', $company_id)->count();
+
+        if ($package > 0) {
+
+            session()->flash("warning", "Insurance package belongs to this company exists, delete request failed");
+            return redirect()->route('sp.company_request');
+        }
+
+
+//  -------------------------- If any packages belongs to this company --------------------------
+
+//  -------------------------- Checking from transaction list --------------------------
+
+        $order_list_check = Order::where('company_id', $company_id)->orWhere('insurance_requested_company_id', $company_id)->count();
+
+        if ($order_list_check > 0) {
+
+            session()->flash("warning", "Insurance transactions belongs to this company exists, delete request failed");
+            return redirect()->route('sp.company_request');
+        }
+
+//  -------------------------- Checking from transaction list --------------------------
+
+//  -------------------------- Insured animal count check --------------------------
+
+        $insured_check = Insured::where('company_id', $company_id)->orWhere('insurance_requested_company_id', $company_id)->count();
+
+        if ($insured_check > 0) {
+
+            session()->flash("warning", "Insurance record belongs to this company exists, delete request failed");
+            return redirect()->route('sp.company_request');
+        }
+
+//  -------------------------- Insured animal count check --------------------------
+
+//  -------------------------- Insurance request status check --------------------------
+
+        $insurance_request_status = InsuranceRequest::where('company_id', $company_id)->orWhere('insurance_requested_company_id', $company_id)->count();
+
+        if ($insurance_request_status > 0) {
+
+            session()->flash("warning", "Insurance request record belongs to this company exists, delete request failed");
+            return redirect()->route('sp.company_request');
+        }
+
+//  -------------------------- Insurance request status check --------------------------
+
+//  -------------------------- Check from cattle reg reports and claim --------------------------
+
+        $cattle_reg_report_and_claim_status = CattleRegReport::where('user_id', $company_id)->count();
+
+        if ($cattle_reg_report_and_claim_status) {
+
+            session()->flash("warning", "Claim report record belongs to this company exists, delete request failed");
+            return redirect()->route('sp.company_request');
+        }
+
+//  -------------------------- Check from cattle reg reports and claim --------------------------
+
+        User::where('company_id', $company_id)->delete();
+        session()->flash("success", "Requested deleted successfully");
+        return redirect()->route('sp.company_request');
 
 
     }
--- resources/views/super-admin/admin-content/company-request/index.blade.php
+++ resources/views/super-admin/admin-content/company-request/index.blade.php
@@ -24,19 +24,42 @@
         </header>
         <!-- Main page content-->
         <div class="container-xl px-4 mt-4">
+
             <!-- Account page navigation-->
             <div class="row">
-
 
                 <div class="col-xl-12">
                     <!-- Account details card-->
                     <div class="card mb-4">
                         <div class="card-header">Company Requests</div>
+
+
                         <div class="card-body">
 
-                            {{--                            <div class="card-header">Extended DataTables</div>--}}
+                            {{-- --------------------------- session data --------------------------- --}}
+
+                            @if(session('success'))
+                                <div class="alert alert-success">
+                                    {{ session('success') }}
+                                </div>
+                            @endif
+
+                            @if(session('warning'))
+                                <div class="alert alert-warning">
+                                    {{ session('warning') }}
+                                </div>
+                            @endif
+
+
+
+                            {{-- --------------------------- session data --------------------------- --}}
+
+
                             <div class="card-body">
+
                                 <table id="datatablesSimple">
+
+
                                     <thead>
                                     <tr>
                                         <td>Serial</td>
